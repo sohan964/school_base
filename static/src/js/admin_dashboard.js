3893ad115acbe872b92166b2e2490277c154e235
sohan964 <sohanurr791@gmail.com>
2026-06-15 15:26:17 +0600

    class routine added at admin dashboard

--- static/src/js/admin_dashboard.js
+++ static/src/js/admin_dashboard.js
@@ -26,6 +26,16 @@ export class SchoolAdminDashboard extends Component {
                 late: 0,
                 absent: 0,
                 attendance_parcent:0
+            },
+            adminRoutineStats: {
+                dayId: null,
+                slotId: null,
+
+                days: [],
+                slots: [],
+
+                routines: [],
+                total: 0,
             }
 
 
@@ -34,11 +44,15 @@ export class SchoolAdminDashboard extends Component {
         this.orm = useService("orm")
 
         onWillStart(async () => {
+            await this.getAccademicYear()
+            await this.getDays()
+            await this.getTimeSlots()
+            await this.getAdminRoutine()
             await this.getAllStudent()
             await this.getAllTeacher()
             await this.getAllClass()
             await this.getAllDepartment()
-            await this.getAccademicYear()
+            
             await this.getTodayAcademicSchedule()
             await this.getTodayAttendanceStats()
         })
@@ -55,6 +69,118 @@ export class SchoolAdminDashboard extends Component {
         let domain = []
         const data = await this.orm.searchCount("school.teacher", domain)
         this.state.teachers.value = data
+    }
+
+    getDays = async () =>{
+        
+        const days = await this.orm.searchRead(
+            "school.weekly.day",
+            [['active', '=', true]],
+            ['name', 'code']
+        );
+        this.state.adminRoutineStats.days = days;
+
+        const todayCode = [
+            'sun',
+            'mon',
+            'tue',
+            'wed',
+            'thu',
+            'fri',
+            'sat'
+        ][new Date().getDay()];
+
+        const currentDay =
+            days.find(d => d.code === todayCode);
+
+        if (currentDay) {
+            this.state.adminRoutineStats.dayId =
+                currentDay.id;
+        }
+    }
+
+    getTimeSlots = async() =>{
+        const slots = await this.orm.searchRead(
+            "school.time.slot",
+            [['active', '=', true]],
+            ['name', 'start_time', 'end_time']
+        );
+
+        this.state.adminRoutineStats.slots = slots;
+
+        const now = new Date();
+
+        const currentHour =
+            now.getHours() +
+            (now.getMinutes() / 60);
+
+        const currentSlot =
+            slots.find(slot =>
+                currentHour >= slot.start_time &&
+                currentHour < slot.end_time
+            );
+
+        if (currentSlot) {
+            this.state.adminRoutineStats.slotId =
+                currentSlot.id;
+        }
+    }
+
+    //routine start
+    onDayChange = async (ev) => {
+
+        this.state.adminRoutineStats.dayId =
+            parseInt(ev.target.value);
+
+        await this.getAdminRoutine();
+    }
+
+    onSlotChange = async (ev) => {
+
+        this.state.adminRoutineStats.slotId =
+            parseInt(ev.target.value);
+
+        await this.getAdminRoutine();
+    }
+
+    getAdminRoutine = async () => {
+
+        let domain = [
+            ['year_id', '=', this.state.accademic_year.id]
+        ];
+        console.log("year need",this.state.accademic_year)
+
+        if (this.state.adminRoutineStats.dayId) {
+            domain.push([
+                'day_id',
+                '=',
+                this.state.adminRoutineStats.dayId
+            ]);
+        }
+
+        if (this.state.adminRoutineStats.slotId) {
+            domain.push([
+                'slot_id',
+                '=',
+                this.state.adminRoutineStats.slotId
+            ]);
+        }
+
+        const data = await this.orm.searchRead(
+            "school.teacher.assignment",
+            domain,
+            [
+                'teacher_id',
+                'subject_id',
+                'class_id',
+                'section_id',
+                'day_id',
+                'slot_id'
+            ]
+        );
+
+        this.state.adminRoutineStats.routines = data;
+        this.state.adminRoutineStats.total = data.length;
     }
 
     getAllClass = async () => {
@@ -86,7 +212,9 @@ export class SchoolAdminDashboard extends Component {
         // console.log(data[0]);
 
         // If you only expect one match:
+        
         this.state.accademic_year = data.length ? data[0] : {};
+        console.log(this.state.accademic_year)
     };
 
     getTodayAcademicSchedule = async () => {
